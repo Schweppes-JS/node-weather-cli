8e33491e78eed74d97ada879f946251e9a34907e
User <user@Users-MacBook-Pro.local>
2023-01-02 23:31:46 +0200

    implemeted getting weather by api

--- weather.js
+++ weather.js
@@ -1,11 +1,28 @@
 #!/usr/bin/env node
 
 import { getArgs } from "./helpers/args.js";
-import { printHelp } from "./services/log.service.js";
+import { getWeather } from "./services/api.service.js";
+import { printHelp, printError, printSuccess } from "./services/log.service.js";
+import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storge.service.js";
+
+const saveToken = async (token) => {
+  if (!token.length) {
+    printError("Token wasn't passed");
+  } else {
+    try {
+      await saveKeyValue(TOKEN_DICTIONARY.token, token);
+      printSuccess("Token was saved");
+    } catch (e) {
+      printError(e.message);
+    }
+  }
+};
 
 const initCLI = () => {
   const args = getArgs(process.argv);
   if (args.h) printHelp();
+  if (args.t) saveToken(args.t);
+  getWeather("London");
 };
 
 initCLI();
